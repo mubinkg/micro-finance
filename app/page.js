1e2a1f40d0c1eef1c9f95324893c0bf1ad7e914d
mubin <mubin.ice.ru@gmail.com>
2024-04-24 22:25:20 +0600

    update interaction

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ import { RightIcon } from "../icons/RightIcon";
 import { CardIcon } from "../icons/CardIcon";
 
 export default function Home() {
+
   return (
     <main>
       <div className="main">
@@ -18,7 +19,8 @@ export default function Home() {
         <Row style={{marginTop: "100px", marginBottom: "100px"}}>
           <h1 style={{textAlign: "center", marginBottom: "100px"}}>How <span style={{textDecoration:"underline"}}>We</span> Work</h1>
           <Col md={6} sm={12} lg={4} content="center">
-            <Content 
+            <Content
+              url="/authentication/registration"
               icon={<LockIcon />} 
               title="SIGN UP" 
               body="Create a quick account. Joining ZimbaCash couldn't be more simple, just enter your email and password." 
@@ -26,6 +28,7 @@ export default function Home() {
           </Col>
           <Col md={6} sm={12} lg={4} content="center">
             <Content
+              url="/authentication/login"
               icon={<RightIcon />}
               title="SUBMIT APPLICATION"
               body="Complete a simple, secure 3 to 5 min online application, no paperwork or phone calls - and get a decision on minutes."
@@ -33,6 +36,7 @@ export default function Home() {
           </Col>
           <Col md={6} sm={12} lg={4} content="center">
             <Content
+              url="/authentication/login"
               icon={<CardIcon />}
               title="GET YOUR MONEY"
               body="Receive your money within minutes. We offer a number of different payment options to suit all clients."
--- components/Content.jsx
+++ components/Content.jsx
@@ -1,6 +1,12 @@
-export default function Content({icon, title, body}) {
+'use client'
+
+import { useRouter } from "next/navigation";
+
+
+export default function Content({icon, title, body, url}) {
+    const router = useRouter()
     return (
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", margin: "0px 40px 0px 40px" }}>
+        <div onClick={()=>router.push(url || "#")} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", margin: "0px 40px 0px 40px", cursor: "pointer" }}>
             {icon}
             <h4 className="mt-4">{title}</h4>
             <p className="mt-3" style={{ textAlign: 'justify' }}>{body}</p>
