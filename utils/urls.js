45c3dc466080808d99c7c731002e3762c6156ce1
mubin <mubin.ice.ru@gmail.com>
2024-05-25 15:46:56 +0600

    add favicon icon

--- utils/urls.js
+++ utils/urls.js
@@ -1,7 +1,7 @@
-const env = 'production';
-// const env = 'dev';
+// const env = 'production';
+const env = 'dev';
 
 export const logoutUrl = (env === 'dev' ? "http://localhost:3000/api/logout" : "http://54.236.12.28/api/logout");
-export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" :  "http://54.236.12.28/backend/api/user/signin");
+export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : "http://54.236.12.28/backend/api/user/signin");
 export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : "http://54.236.12.28/api/login");
-export const baseUrl = (env === 'dev' ? 'http://localhost:3001': 'http://54.236.12.28/backend/api/');
+export const baseUrl = (env === 'dev' ? 'http://localhost:3001':'http://54.236.12.28/backend/api/');
