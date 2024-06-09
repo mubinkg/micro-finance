5570243497eba1a55d24e1382db22d2418adce36
mubin <mubin.ice.ru@gmail.com>
2024-06-09 23:53:02 +0600

    udpate prod url

--- utils/urls.js
+++ utils/urls.js
@@ -1,8 +1,8 @@
 const env = 'production';
 // const env = 'dev';
 
-export const logoutUrl = (env === 'dev' ? "http://localhost:3000/api/logout" : "http://54.236.12.28/api/logout");
-export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : "http://54.236.12.28/backend/api/user/signin");
-export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : "http://54.236.12.28/api/login");
-export const baseUrl = (env === 'dev' ? 'http://localhost:3001':'http://54.236.12.28/backend/api/');
+export const logoutUrl = (env === 'dev' ? "http://localhost:3000/api/logout" : "http://54.196.46.131/api/logout");
+export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : "http://54.196.46.131/backend/api/user/signin");
+export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : "http://54.196.46.131/api/login");
+export const baseUrl = (env === 'dev' ? 'http://localhost:3001':'http://54.196.46.131/backend/api/');
 export const totalLoanUrl = `${baseUrl}/loan/total-approved-loan`
