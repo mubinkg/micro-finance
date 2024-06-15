04e6d4efe7dadfb67aaadec1f229870fb6b70cb6
mubin <mubin.ice.ru@gmail.com>
2024-06-15 23:53:54 +0600

    udpate url

--- utils/urls.js
+++ utils/urls.js
@@ -1,8 +1,8 @@
 const env = 'production';
 // const env = 'dev';
 
-export const logoutUrl = (env === 'dev' ? "http://localhost:3000/api/logout" : "https:www.zimbacash.com/api/logout");
-export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : "https:www.zimbacash.com/backend/api/user/signin");
-export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : "https:www.zimbacash.com/api/login");
-export const baseUrl = (env === 'dev' ? 'http://localhost:3001':'https:www.zimbacash.com/backend/api');
+export const logoutUrl = (env === 'dev' ? "http://localhost:3000/api/logout" : "https://www.zimbacash.com/api/logout");
+export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : "https://www.zimbacash.com/backend/api/user/signin");
+export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : "https://www.zimbacash.com/api/login");
+export const baseUrl = (env === 'dev' ? 'http://localhost:3001':'https://www.zimbacash.com/backend/api');
 export const totalLoanUrl = `${baseUrl}/loan/total-approved-loan`
