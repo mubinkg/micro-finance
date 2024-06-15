c50cb90c4109b3d20ff7fab0e28ccc34c36f2f0d
mubin <mubin.ice.ru@gmail.com>
2024-06-15 22:59:13 +0600

    update base url

--- app/api/logout/route.js
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-
-export async function GET(){
-    const cookie = cookies()
-    cookie.delete('access_token')
-    return new Response(JSON.stringify({
-        'message': 'Delete'
-    }))
-}
--- utils/urls.js
+++ utils/urls.js
@@ -4,5 +4,5 @@ const env = 'production';
 export const logoutUrl = (env === 'dev' ? "http://localhost:3000/api/logout" : "https:www.zimbacash.com/api/logout");
 export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : "https:www.zimbacash.com/backend/api/user/signin");
 export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : "https:www.zimbacash.com/api/login");
-export const baseUrl = (env === 'dev' ? 'http://localhost:3001':'https:www.zimbacash.com/backend/api/');
+export const baseUrl = (env === 'dev' ? 'http://localhost:3001':'https:www.zimbacash.com/backend/api');
 export const totalLoanUrl = `${baseUrl}/loan/total-approved-loan`
