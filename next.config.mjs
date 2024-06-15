d760abe9611fe6bd7cbf5f38a0d34db1941ca4c3
mubin <mubin.ice.ru@gmail.com>
2024-06-15 23:07:19 +0600

    udpate config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    crossOrigin: 'anonymous',
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
     images: {
         remotePatterns: [{
           protocol: 'https',
