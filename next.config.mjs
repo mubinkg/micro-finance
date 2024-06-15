956cd0f379b599e8a621ac415185ecdcd97315de
mubin <mubin.ice.ru@gmail.com>
2024-06-15 23:06:20 +0600

    udpate config

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+          protocol: 'https',
+          hostname: 'zimbacash-bucket.s3.amazonaws.com',
+          port: '',
+          pathname: '/**'
+        }]
+      },
+}
 
+export default nextConfig;
