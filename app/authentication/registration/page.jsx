426182933df1efabd39354b9c8aa23c723278897
mubin <mubin.ice.ru@gmail.com>
2024-07-15 00:19:50 +0600

    udpate registration message

--- app/authentication/registration/page.jsx
+++ app/authentication/registration/page.jsx
@@ -58,7 +58,7 @@ export default function Page() {
         }).catch(err=>{
             Swal.fire({
                 title: 'Registration',
-                text:'Error on register new user.',
+                text:'Error on registering new user.',
                 icon: "error"
             })
         })
