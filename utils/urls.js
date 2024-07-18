6fd6f39e595206dab671de480d7f80faddde5850
mubin <mubin.ice.ru@gmail.com>
2024-07-18 18:22:58 +0600

    udpate url

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
-// const env = 'production';
-const env = 'dev';
+const env = 'production';
+// const env = 'dev';
 
 export const logoutUrl = (env === 'dev' ? "http://localhost:3000/api/logout" : "https://www.zimbacash.com/api/logout");
 export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : "https://www.zimbacash.com/backend/api/user/signin");
