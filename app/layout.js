25771424ed01a5b4b9cba4ef7d4b2bb5fe4f0eba
mubin <mubin.ice.ru@gmail.com>
2024-07-25 09:05:20 +0600

    add mouseflow

--- app/layout.js
+++ app/layout.js
@@ -30,9 +30,18 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="google-site-verification" content="ZU1VWY_mpCbIdH0IZEqzEAFwCy8j7xDd1TECex57P6M" />
         <meta property="og:image" content="/L-1.jpg" />
+        <script dangerouslySetInnerHTML={{
+        __html: `window._mfq = window._mfq || [];
+          (function() {
+            var mf = document.createElement("script");
+            mf.type = "text/javascript"; mf.defer = true;
+            mf.src = "//cdn.mouseflow.com/projects/501d15bd-cda8-432a-9572-04a0f190eced.js";
+            document.getElementsByTagName("head")[0].appendChild(mf);
+          })();`}}>
+      </script>
       </head>
       <body className={`${inter.className}`}>{children}</body>
-      <Footer/>
+      <Footer />
     </html>
   );
 }
