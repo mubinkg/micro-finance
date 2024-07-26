e9792e6a769c1b5ec7670ead04c76de9d464f411
mubin <mubin.ice.ru@gmail.com>
2024-07-26 20:23:19 +0600

    udpate uat url

--- utils/urls.js
+++ utils/urls.js
@@ -6,12 +6,12 @@ export const logoutUrl = (
     env === 'dev' 
     ? "http://localhost:3000/api/logout" 
     : env === 'uat' 
-    ? "http://52.201.228.135/api/logout" 
+    ? "https://dev.zimbacash.com/api/logout" 
     : "https://www.zimbacash.com/api/logout"
 );
 
-export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : env === 'uat' ? "http://52.201.228.135/backend/api/user/signin" : "https://www.zimbacash.com/backend/api/user/signin");
+export const signinApiUrl =  (env === 'dev' ? "http://localhost:3001/user/signin" : env === 'uat' ? "https://dev.zimbacash.com/backend/api/user/signin" : "https://www.zimbacash.com/backend/api/user/signin");
 
-export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : env === 'uat'? "http://52.201.228.135/api/login" : "https://www.zimbacash.com/api/login");
-export const baseUrl = (env === 'dev' ? 'http://localhost:3001': env === 'uat' ? "http://52.201.228.135/backend/api" :'https://www.zimbacash.com/backend/api');
+export const signinUrl = (env === 'dev' ? "http://localhost:3000/api/login" : env === 'uat'? "https://dev.zimbacash.com/api/login" : "https://www.zimbacash.com/api/login");
+export const baseUrl = (env === 'dev' ? 'http://localhost:3001': env === 'uat' ? "https://dev.zimbacash.com/backend/api" :'https://www.zimbacash.com/backend/api');
 export const totalLoanUrl = `${baseUrl}/loan/total-approved-loan`
