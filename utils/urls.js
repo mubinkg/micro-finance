e1cd74d9f754557f5b532df4d553c07840b4758a
Mubin Mohammad Ikhtiar Khan <129994262+mubinkg@users.noreply.github.com>
2025-07-19 14:58:48 +0600

    udpate reset password

--- utils/urls.js
+++ utils/urls.js
@@ -2,5 +2,5 @@
 const env = 'dev';
 // const env = 'uat'
 
-export const baseUrl = "http://localhost:3002"
+export const baseUrl = "http://127.0.0.1:3001"
 export const totalLoanUrl = `${baseUrl}/loan/total-approved-loan`
